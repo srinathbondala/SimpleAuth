8006f7df3f902ba1b58fc78d73de4aa8ef073307
Srinath Bondala <srinath.b2004@gmail.com>
2026-02-11 17:28:13 +0530

    docker changes

--- src/main/java/com/authservice/simple_auth/Configure/Interceptors/UserStatusInterceptor.java
+++ src/main/java/com/authservice/simple_auth/Configure/Interceptors/UserStatusInterceptor.java
@@ -10,7 +10,7 @@ import jakarta.servlet.http.HttpServletResponse;
 @Component
 public class UserStatusInterceptor implements HandlerInterceptor {
 
-    @Autowired
+    // @Autowired
     // private authservice authService;
 
     @Override
